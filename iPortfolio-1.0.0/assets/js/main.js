c84c3f78c440fe88ccf5ca9859d7b09992f38ffa
Bradley Pearce <brad1506@icloud.com>
2025-05-10 01:57:05 +0100

    Final 6.0

--- iPortfolio-1.0.0/assets/js/main.js
+++ iPortfolio-1.0.0/assets/js/main.js
@@ -239,7 +239,7 @@
 
 const counter = document.querySelector(".counter-number");
 async function updateCounter() {
-  let response = await fetch("https://fnl5tkt2s6wwxaxvbpf43rcoi40lvuqr.lambda-url.eu-west-2.on.aws/");
+  let response = await fetch("https://cton0dd0rk.execute-api.eu-west-2.amazonaws.com/get-views");
   let data = await response.json();
   counter.innerHTML = `Views: ${data}`;
 }
